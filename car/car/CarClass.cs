9da223f26e20568fe1437c7f01cd7923adc8bfc9
JanneP80 <janne.puolitaival@student.saimia.fi>
2017-02-27 10:24:40 +0200

    car is the first oop test Program BankObjects is first actual oop task

---
+++ car/car/CarClass.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car
+{
+    public class CarClass // halutaan publiciksi - on näykyvissä sovelluksessa myös muualla
+    {
+        private readonly string _brand; // ei sitten mene sekaisin metodeissa samannimisen muuttujan kanssa, yrityksissä valitaan
+        private double _speed;
+        // constructor 
+
+        public CarClass(string brand)
+        {
+            _brand = brand;
+            _speed = 0; // ei kannata tuoda parametriksi, voidaan pistää construktorin sisään defaulttina nollaksi
+        }
+        // vaihtoehtoinen oletuskonstruktori
+        // car () {}
+
+        // tehdään brand muuttujasta property eli ... alt ja insert
+
+            // construktorin jälkeen tulee ---metodit, sitten publikit ja sitten privatet
+        public string Brand 
+        {
+            get { return _brand + " Blaah"; }
+            // set { brand = value; } //koska auton merkki ei enää muutu, niin tän voi ottaa pois
+        }
+
+        public double Speed { get; private set;
+            // set { _speed = value; } // pois -> nyt vain acceleraten kautta voi muuttaa nopeutta
+        }
+
+        public void Accelerate(double acceleration)
+        {
+            Speed = Speed + acceleration;
+        }
+
+        public void Break()
+        {
+            Speed = Speed * .9;
+        }
+
+        // ylikirjoitetaan defaulttimetodi tostring täällä car luokassa
+        public override string ToString()
+        {
+            // return base.ToString(); // tekee näin defaulttina
+            //return "Brand: " + _brand +", "+ " Speed " + Speed; // suoraan muuttujan kautta, jos mentäis propertyn kautta niin tulostuisi siellä lisättä "blaah"
+            // jos haluaa logittaa tai tulostaa tietoja muuttujasta niin kannattaa käyttää tostringin ylikirjoitusta
+            return Print();
+        }
+
+        private string Print() // voi kutsua vain täällä
+        {
+            return "Brand: " + _brand + ", " + " Speed " + Speed;
+        }
+    }
+}
---
+++ car/car/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            // olio on luokan instanssi/ilmentymä/toteutus/
+            var carClass = new CarClass("Volvo"); // constructorissa parametrissa mukana, joten asettaa volvoksi tässä
+            // carClass.Brand = "Honda"; // setillä asettaa uuden arvon. mutta ei enää mahdollista.
+            Console.WriteLine("Brand: " + carClass.Brand); // kutsuu get funktioa. jos halutaan käyttää set niin yllä
+            Console.WriteLine("Default speed: " + carClass.Speed);
+            // carClass.Speed = 100; muutttui readonlyksi eikä toimi enää
+            carClass.Accelerate(20);
+            carClass.Accelerate(10);
+            Console.WriteLine("New speed: " + carClass.Speed);
+            carClass.Break();
+            Console.WriteLine("Speed after break: " + carClass.Speed);
+            Console.WriteLine(carClass); // käyttää automaattisesti tostringia ilman erillistä kutsua
+            Console.ReadKey();
+        }
+    }
+}
