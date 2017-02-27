a711c9e63a00485a93cd318f387ced31f89891c9
JanneP80 <janne.puolitaival@student.saimia.fi>
2017-02-27 11:07:36 +0200

    ho

--- bankobjects/BankObjects/CustomerObject.cs
+++ bankobjects/BankObjects/CustomerObject.cs
@@ -8,5 +8,39 @@ namespace BankObjects
 {
     class CustomerObject
     {
+        private string _firstname;
+        private string _lastname;
+        private string _accountnumber;
+
+
+        public CustomerObject(string firstname)
+        {
+            _firstname = Firstname;
+            _lastname = Lastname;
+            _accountnumber = Accountnumber;
+        }
+
+        public string Firstname
+        {
+            get { return _firstname; }
+            set { _firstname = value; }
+        }
+
+        public string Lastname
+        {
+            get { return _lastname; }
+            set { _lastname = value; }
+        }
+
+        public string Accountnumber
+        {
+            get { return _accountnumber; }
+            set { _accountnumber = value; }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
