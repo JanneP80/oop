a109df9e527f789a80831b4af23f08e54ac217ca
JanneP80 <janne.puolitaival@student.saimia.fi>
2017-03-03 17:34:42 +0200

    save

--- skijumpscoreboard/SkiJumpScoreBoard/Form1.cs
+++ skijumpscoreboard/SkiJumpScoreBoard/Form1.cs
@@ -14,9 +14,11 @@ namespace SkiJumpScoreBoard
     {
         public SkiJumpMainForm()
         {
+            InitializeComponent();
+
             List<SkiJumper> skiJumpers = new List<SkiJumper>();
             int HillSize = 0;
-
+            // int n = 0;
 
             skiJumpers.Add(new SkiJumper(1, "Adam Malycz", 0));
             skiJumpers.Add(new SkiJumper(2, "Martin Schmitdt", 0));
@@ -27,11 +29,30 @@ namespace SkiJumpScoreBoard
             comboBox1.DataSource = skiJumpers;
             comboBox1.DisplayMember = "competitorName";
 
-
+            var currentJumper = comboBox1.SelectedIndex;
+            int tempvar = Convert.ToInt32(textBox1.Text);
+            skiJumpers[currentJumper].Points = tempvar;
+            //skiJumpers[currentJumper].Points = textBox1.Text;
+            if (skiJumpers[currentJumper].Points < 1)
+            {
+                textBox1.AppendText("Please insert number greater than one. ");
+                //textBox1.
+            } /*
+            else if (n < 1)
+            {
+                textBox1.Text="Please insert number greater than one. "
+            }*/
         }
 
         private void SkiJumpMainForm_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //Jump Length
+            // 
 
         }
     }
--- skijumpscoreboard/SkiJumpScoreBoard/SkiJumper.cs
+++ skijumpscoreboard/SkiJumpScoreBoard/SkiJumper.cs
@@ -14,5 +14,23 @@
             _competitorName = competitorName;
             _points = points;
         }
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string CompetitorName
+        {
+            get { return _competitorName; }
+            set { _competitorName = value; }
+        }
+
+        public int Points
+        {
+            get { return _points; }
+            set { _points = value; }
+        }
     }
 }
